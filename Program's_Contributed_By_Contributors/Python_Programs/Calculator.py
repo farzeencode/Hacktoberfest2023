63f576907e3c271c0f1ce56c637d4aba764ce155
farzeencode <145785539+farzeencode@users.noreply.github.com>
2023-10-01 15:58:39 +0530

    Update Calculator.py
    i just updated the calculator

--- Program's_Contributed_By_Contributors/Python_Programs/Calculator.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Calculator.py
@@ -1,119 +1,51 @@
-from tkinter import *
-import math
+# Function to perform addition
+def add(x, y):
+    return x + y
 
-root = Tk()
-root.title('CalcX')
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
 
-e = Entry(root, width=35, borderwidth=4)
-e.grid(column=0, row=0, columnspan=3)
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
 
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
 
-def ln():
-    s = int(e.get())
-    e.delete(0, END)
-    try:
-        e.insert(0, str(math.log(s)))
-    except Exception as z:
-        e.insert(0, z)
+# Main function
+def main():
+    while True:
+        # Display the menu
+        print("Options:")
+        print("Enter 'add' for addition")
+        print("Enter 'subtract' for subtraction")
+        print("Enter 'multiply' for multiplication")
+        print("Enter 'divide' for division")
+        print("Enter 'quit' to end the program")
 
-def fact():
-    x = int(e.get())
-    e.delete(0, END)
-    if x >= 1:
-        val = 1
-        for i in range(x, 1, -1):
-            val *= i
-        e.insert(0, str(val))
+        # User input
+        user_input = input(": ")
 
-    else:
-        e.insert(0, 'how dumb can you be??!')
+        if user_input == "quit":
+            break
+        elif user_input in ("add", "subtract", "multiply", "divide"):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-def f_num(n):
-    x = e.get()
-    e.delete(0, END)
-    e.insert(0, str(x) + str(n))
+            if user_input == "add":
+                print("Result:", add(num1, num2))
+            elif user_input == "subtract":
+                print("Result:", subtract(num1, num2))
+            elif user_input == "multiply":
+                print("Result:", multiply(num1, num2))
+            elif user_input == "divide":
+                print("Result:", divide(num1, num2))
+        else:
+            print("Invalid input. Please try again.")
 
-def f_clear():
-    e.delete(0, END)
-
-def f_op(s = None):
-    x = e.get()
-    e.delete(0, END)
-    global first_n , sign
-    try:
-        first_n = int(x)
-    except:
-        e.insert(0, 'Syntax Error')
-    sign = s
-
-def f_equal():
-    a = e.get()
-    e.delete(0, END)
-    if a.isnumeric():
-        if sign == '+':
-                e.insert(0, str(int(a) + first_n))
-        elif sign == 'x':
-                e.insert(0,str(int(a) * first_n))
-
-        elif sign == '-':
-                e.insert(0, str(first_n - int(a)))
-
-        elif sign == '/':
-            try:
-                e.insert(0, str(first_n/int(a)))
-            except:
-                e.insert(0, 'Now why would do that..')
-        elif sign == 'e':
-            e.insert(0, str(first_n**int(a)))
-
-        elif sign == None:
-            e.insert(str(a))
-
-    elif a=='':
-        pass
-
-    else:
-        e.insert(0, 'Syntax Error')
-
-
-button_1 = Button(root, text='1', command=lambda: f_num(1), padx=40, pady=20)
-button_2 = Button(root, text='2', command=lambda: f_num(2), padx=40, pady=20)
-button_3 = Button(root, text='3', command=lambda: f_num(3), padx=40, pady=20)
-button_4 = Button(root, text='4', command=lambda: f_num(4), padx=40, pady=20)
-button_5 = Button(root, text='5', command=lambda: f_num(5), padx=40, pady=20)
-button_6 = Button(root, text='6', command=lambda: f_num(6), padx=40, pady=20)
-button_7 = Button(root, text='7', command=lambda: f_num(7), padx=40, pady=20)
-button_8 = Button(root, text='8', command=lambda: f_num(8), padx=40, pady=20)
-button_9 = Button(root, text='9', command=lambda: f_num(9), padx=40, pady=20)
-button_0 = Button(root, text='0', command=lambda: f_num(0), padx=40, pady=20)
-button_e = Button(root, text='=', command=lambda: f_equal(), padx=39, pady=20)
-button_a = Button(root, text='+', command=lambda: f_op('+'), padx=39, pady=20)
-button_m = Button(root, text='x', command=lambda: f_op('x'), padx=40, pady=20)
-button_d = Button(root, text='/', command=lambda: f_op('/'), padx=40, pady=20)
-button_s = Button(root, text='-', command=lambda: f_op('-'), padx=40, pady=20)
-button_ln = Button(root, text='ln', command=ln, padx=38, pady=20)
-button_fact = Button(root, text='!', command=fact, padx=41, pady=20)
-button_exp = Button(root, text='^', command=lambda: f_op('e'), padx=39, pady=20)
-button_c = Button(root, text='CLEAR', command=f_clear, padx=125, pady=20)
-
-button_1.grid(column=0, row=1)
-button_2.grid(column=1, row=1)
-button_3.grid(column=2, row=1)
-button_4.grid(column=0, row=2)
-button_5.grid(column=1, row=2)
-button_6.grid(column=2, row=2)
-button_7.grid(column=0, row=3)
-button_8.grid(column=1, row=3)
-button_9.grid(column=2, row=3)
-button_0.grid(column=0, row=4)
-button_e.grid(column=1, row=4)
-button_a.grid(column=2, row=4)
-button_m.grid(column=0, row=5)
-button_d.grid(column=1, row=5)
-button_s.grid(column=2, row=5)
-button_ln.grid(column=0, row=6)
-button_fact.grid(column=1, row=6)
-button_exp.grid(column=2, row=6)
-button_c.grid(column=0, row=7, columnspan=3)
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
